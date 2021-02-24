67f2e01db2e862e0fb70a4bb6a9e29bea250954a
FeriBolour <57817579+FeriBolour@users.noreply.github.com>
2021-02-24 15:47:56 -0600

    Create HandGestureExtraction.py

---
+++ HandGestureExtraction.py
@@ -0,0 +1,106 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb 24 15:04:55 2021
+
+@author: farsh
+"""
+
+import cv2
+import numpy as np
+import time
+
+# General Settings
+prediction = ''
+action = ''
+score = 0
+img_counter = 500
+
+
+# pygame.event.wait()
+
+class Volume(object):
+    def __init__(self):
+        self.level = .5
+
+    def increase(self, amount):
+        self.level += amount
+        print(f'New level is: {self.level}')
+
+    def decrease(self, amount):
+        self.level -= amount
+        print(f'New level is: {self.level}')
+
+
+vol = Volume()
+
+
+
+# parameters
+cap_region_x_begin = 0.5  # start point/total width
+cap_region_y_end = 0.8  # start point/total width
+threshold = 60  # binary threshold
+blurValue = 41  # GaussianBlur parameter
+bgSubThreshold = 50
+learningRate = 0
+
+# variableslt
+isBgCaptured = 0  # bool, whether the background captured
+triggerSwitch = False  # if true, keyboard simulator works
+
+
+def remove_background(frame):
+    fgmask = bgModel.apply(frame, learningRate=learningRate)
+    kernel = np.ones((3, 3), np.uint8)
+    fgmask = cv2.erode(fgmask, kernel, iterations=1)
+    res = cv2.bitwise_and(frame, frame, mask=fgmask)
+    return res
+
+
+# Camera
+camera = cv2.VideoCapture(0)
+camera.set(10, 200)
+
+while camera.isOpened():
+    ret, frame = camera.read()
+    frame = cv2.bilateralFilter(frame, 5, 50, 100)  # smoothing filter
+    frame = cv2.flip(frame, 1)  # flip the frame horizontally
+    cv2.rectangle(frame, (int(cap_region_x_begin * frame.shape[1]), 0),
+                  (frame.shape[1], int(cap_region_y_end * frame.shape[0])), (255, 0, 0), 2)
+
+    cv2.imshow('original', frame)
+
+    # Run once background is captured
+    if isBgCaptured == 1:
+        img = remove_background(frame)
+        img = img[0:int(cap_region_y_end * frame.shape[0]),
+              int(cap_region_x_begin * frame.shape[1]):frame.shape[1]]  # clip the ROI
+        cv2.imshow('mask', img)
+
+        # convert the image into binary image
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        blur = cv2.GaussianBlur(gray, (blurValue, blurValue), 0)
+        cv2.imshow('blur', blur)
+        ret, thresh = cv2.threshold(blur, threshold, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+
+        cv2.imshow('ori', thresh)
+
+
+    # Keyboard OP
+    k = cv2.waitKey(10)
+    if k == 27:  # press ESC to exit all windows at any time
+        break
+    elif k == ord('b'):  # press 'b' to capture the background
+        bgModel = cv2.createBackgroundSubtractorMOG2(0, bgSubThreshold)
+        time.sleep(2)
+        isBgCaptured = 1
+        print('Background captured')
+
+    elif k == ord('r'):  # press 'r' to reset the background
+        time.sleep(1)
+        bgModel = None
+        triggerSwitch = False
+        isBgCaptured = 0
+        print('Reset background')
+
+
+    
